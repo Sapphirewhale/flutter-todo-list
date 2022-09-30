6634125cd26a12067bfbce141d7eff6323aff5ff
Sami Yildirim <sapphirewhalez@gmail.com>
2022-09-30 16:18:35 +1000

    Merge main and dev dependencies

--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,20 +2,9 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:tame_the_beast/auth_provider.dart';
 import 'package:tame_the_beast/firebase_options.dart';
 import 'package:tame_the_beast/homepage.dart';
-import 'package:tame_the_beast/repositories/todo_list_item_repository.dart';
-import 'package:tame_the_beast/repositories/user_repository.dart';
-
-void initDependencies() {
-  Get.put(FirebaseFirestore.instance);
-  Get.put(FirebaseAuth.instance);
-  Get.put(AuthProvider());
-  Get.put(UserRepository());
-  Get.put(TodoListItemRepository());
-}
+import 'package:tame_the_beast/main.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
