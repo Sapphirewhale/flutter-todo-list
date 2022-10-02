98f74484bace8a697869aee08dea4dd68a1bbc51
Sami Yildirim <sapphirewhalez@gmail.com>
2022-10-02 19:29:08 +1100

    Reorganize classes

--- lib/todays_list_screen.dart
+++ lib/todays_list_screen.dart
@@ -3,7 +3,7 @@ import 'package:get/get.dart';
 import 'package:tame_the_beast/auth_provider.dart';
 import 'package:tame_the_beast/models/todo_list_item.dart';
 import 'package:tame_the_beast/repositories/todo_list_item_repository.dart';
-import 'package:tame_the_beast/theme.dart';
+import 'package:tame_the_beast/widgets/todo_list_section.dart';
 
 class TodaysListScreen extends StatelessWidget {
   const TodaysListScreen({Key? key}) : super(key: key);
@@ -104,134 +104,6 @@ class TodaysProgressDisplay extends StatelessWidget {
           ),
         ],
       ),
-    );
-  }
-}
-
-class TodoListItemDisplay extends StatelessWidget {
-  final TodoListItem item;
-  TodoListItemDisplay({required this.item, Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: [
-          Text(
-            item.title,
-            style: item.isDone
-                ? TextStyle(decoration: TextDecoration.lineThrough)
-                : null,
-          ),
-          Checkbox(
-              value: item.isDone,
-              onChanged: (val) {
-                item.toggleTaskCompletion();
-                Get.find<TodoListItemRepository>().saveItem(item);
-              })
-        ],
-      ),
-    );
-  }
-}
-
-class TodoListTitleDisplay extends StatelessWidget {
-  final TodoListItemType type;
-  final int count;
-  const TodoListTitleDisplay(
-      {required this.type, required this.count, Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-        padding: EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: type.color,
-          borderRadius: BorderRadius.circular(10),
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Container(
-              padding: EdgeInsets.all(7),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(25),
-                color: TTBTheme.transparentWhite,
-              ),
-              child: SizedBox(width: 100, child: Align(child: Text(type.name))),
-            ),
-            Container(
-                padding: EdgeInsets.all(7),
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  color: TTBTheme.transparentWhite,
-                ),
-                child: Text(count.toString())),
-            InkWell(
-              child: Container(
-                  padding: EdgeInsets.all(7),
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    color: TTBTheme.transparentWhite,
-                  ),
-                  child: Text('+')),
-              onTap: () => addTodoListItem(context),
-            )
-          ],
-        ));
-  }
-
-  void addTodoListItem(BuildContext context) {
-    TextEditingController controller = TextEditingController();
-    showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            title: Text('Add Todo List Item'),
-            content: TextField(
-              controller: controller,
-              decoration: InputDecoration(hintText: 'Enter a title'),
-            ),
-            actions: [
-              TextButton(
-                  onPressed: () => Navigator.pop(context),
-                  child: Text('Cancel')),
-              TextButton(
-                  onPressed: () {
-                    Get.find<TodoListItemRepository>()
-                        .saveItem(SingleTodoListItem(
-                      title: controller.text,
-                      appUserId: Get.find<AuthProvider>().getUserId(),
-                    ));
-                    Navigator.pop(context);
-                  },
-                  child: Text('Save')),
-            ],
-          );
-        });
-  }
-}
-
-class TodoListSection extends StatelessWidget {
-  final TodoListItemType type;
-  final List<TodoListItem> items;
-  const TodoListSection({required this.type, required this.items, Key? key})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TodoListTitleDisplay(type: type, count: items.length),
-        for (var item in items.where((element) => !element.isDone))
-          TodoListItemDisplay(item: item),
-        for (var item in items.where((element) =>
-            element.isDone &&
-            element.completionDate!
-                .isAfter(DateTime.now().subtract(Duration(days: 1)))))
-          TodoListItemDisplay(item: item)
-      ],
     );
   }
 }
---
+++ lib/widgets/todo_list_section.dart
@@ -0,0 +1,136 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../auth_provider.dart';
+import '../models/todo_list_item.dart';
+import '../repositories/todo_list_item_repository.dart';
+import '../theme.dart';
+
+class TodoListSection extends StatelessWidget {
+  final TodoListItemType type;
+  final List<TodoListItem> items;
+  const TodoListSection({required this.type, required this.items, Key? key})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    DateTime now = DateTime.now();
+    return Column(
+      children: [
+        TodoListTitleDisplay(type: type, count: items.length),
+        for (var item in items.where((element) => !element.isDone))
+          TodoListItemDisplay(item: item),
+        for (var item in items.where((element) =>
+            element.isDone &&
+            element.completionDate!
+                .isAfter(DateTime(now.year, now.month, now.day))))
+          TodoListItemDisplay(item: item)
+      ],
+    );
+  }
+}
+
+class TodoListItemDisplay extends StatelessWidget {
+  final TodoListItem item;
+  TodoListItemDisplay({required this.item, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Row(
+        children: [
+          Text(
+            item.title,
+            style: item.isDone
+                ? TextStyle(decoration: TextDecoration.lineThrough)
+                : null,
+          ),
+          Checkbox(
+              value: item.isDone,
+              onChanged: (val) {
+                item.toggleTaskCompletion();
+                Get.find<TodoListItemRepository>().saveItem(item);
+              })
+        ],
+      ),
+    );
+  }
+}
+
+class TodoListTitleDisplay extends StatelessWidget {
+  final TodoListItemType type;
+  final int count;
+  const TodoListTitleDisplay(
+      {required this.type, required this.count, Key? key})
+      : super(key: key);
+
+  void addTodoListItem(BuildContext context) {
+    TextEditingController controller = TextEditingController();
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Text('Add Todo List Item'),
+            content: TextField(
+              controller: controller,
+              decoration: InputDecoration(hintText: 'Enter a title'),
+            ),
+            actions: [
+              TextButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: Text('Cancel')),
+              TextButton(
+                  onPressed: () {
+                    Get.find<TodoListItemRepository>()
+                        .saveItem(SingleTodoListItem(
+                      title: controller.text,
+                      appUserId: Get.find<AuthProvider>().getUserId(),
+                    ));
+                    Navigator.pop(context);
+                  },
+                  child: Text('Save')),
+            ],
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          color: type.color,
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: EdgeInsets.all(7),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(25),
+                color: TTBTheme.transparentWhite,
+              ),
+              child: SizedBox(width: 100, child: Align(child: Text(type.name))),
+            ),
+            Container(
+                padding: EdgeInsets.all(7),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: TTBTheme.transparentWhite,
+                ),
+                child: Text(count.toString())),
+            InkWell(
+              child: Container(
+                  padding: EdgeInsets.all(7),
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: TTBTheme.transparentWhite,
+                  ),
+                  child: Text('+')),
+              onTap: () => addTodoListItem(context),
+            )
+          ],
+        ));
+  }
+}
